05b0d8adccee7aa1cc3bac118f92576b1e9f9df6
Benjamin Dutton <gbendutton@gmail.com>
2020-02-13 14:05:04 +0800

    Add auto updating

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Azure Nav Highlighter
 // @namespace    https://github.com/BenDutton/AzureNavHighlighter
-// @version      1.0.0
+// @version      1.1.0
 // @description  Highlight the Azure nav bar based off the current environment
 // @author       Benjamin Dutton
 // @match        https://*.portal.azure.com/*
+// @updateURL    https://raw.githubusercontent.com/BenDutton/AzureNavHighlighter/master/script.js
+// @downloadURL  https://raw.githubusercontent.com/BenDutton/AzureNavHighlighter/master/script.js
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
